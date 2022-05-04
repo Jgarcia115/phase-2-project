ed6a4e0450250aafc74825c0f0b41d4592c97381
Jonathan <jono_95@yahoo.com>
2022-05-03 20:09:44 -0400

    forgotten

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import CardList from './CardList';
+import NavBar from './NavBar';
+import Header from './Header';
+import Mission from './Mission';
+import { Router, Routes, Route } from "react-router-dom";
+import AddCard from './AddCard';
+
 
 function App() {
+
+  const [toys, setToys] = useState([]) 
+
+  function addToy(newToy) {
+    const updatedToys = [...toys, newToy]
+    setToys(updatedToys)
+  }
 
   useEffect(()=> {
     fetch("http://localhost:3000/toys")
     .then(r=> r.json())
     .then(data => {
-      console.log(data)
+      setToys(data)
     })
   }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
+      <Header/>
+      <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Mission />}/>
+            <Route path="/known" element={<CardList toys={toys} />}/>
+            <Route path="/add" element={<AddCard onAddToy={addToy}/>}/>
+          </Routes>
+      </div>
     </div>
   );
 }
