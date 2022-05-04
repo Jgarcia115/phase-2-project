6bab96cb742cb20b0cafd8a0c42285e157f45e26
Jonathan <jono_95@yahoo.com>
2022-05-03 20:12:31 -0400

    Added a Mission component

---
+++ src/Mission.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Mission() {
+    return (
+        <div className='About'>
+          <h1>Mission Statement</h1>
+          <p className="statement">We intend to document and capture any and all living, talking dolls to be examined and categorized. This is a crowdsourced Database to keep track and add new toys to the list of known toys</p>
+        </div>
+    )
+}
+
+export default Mission;
