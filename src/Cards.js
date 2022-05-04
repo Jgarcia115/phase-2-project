48ee63126fa4ffe47815dcebcc5b7cb099791c0d
Jonathan <jono_95@yahoo.com>
2022-05-03 20:11:53 -0400

    Added Cards component

---
+++ src/Cards.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Cards({ toy }) {
+    const { id, name, likes, image} = toy
+
+
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <img src={image} alt='toy photo' width="250" height="250" />
+                </div>
+        </div>
+        </div>
+    )
+}
+
+export default Cards;
